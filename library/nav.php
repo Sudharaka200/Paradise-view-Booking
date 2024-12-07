57821a81962bf819126cb4c514d39d3780528db9
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2024-12-08 01:15:57 +0530

    Update nav.php

--- library/nav.php
+++ library/nav.php
@@ -7,23 +7,23 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../library/home.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="../home/index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../library/Pages/explore.php">Explore</a>
+          <a class="nav-link" href="../Pages/explore.php">Explore</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../library/pages/rooms.php">Rooms</a>
+          <a class="nav-link" href="../pages/rooms.php">Rooms</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../library//pages/about.php">About</a>
+          <a class="nav-link" href="..//pages/about.php">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../library/Pages/contact.php">Contact</a>
+          <a class="nav-link" href="../Pages/contact.php">Contact</a>
         </li>
       </ul>
       <span class="navbar-text">
-        <a href="../library/Pages/booknow.php"><button class="btn-nav-book">Book Now</button></a>
+        <a href="../Pages/booknow.php"><button class="btn-nav-book">Book Now</button></a>
       </span>
     </div>
   </div>
